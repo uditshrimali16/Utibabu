adaa81edcb9409e16215f3aba0da7c3f962247c0
uditshrimali16 <uditshrimali16@gmail.com>
2025-03-24 17:51:31 +0530

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '7387812337:AAHthFwqG5RFYbgmfLEW8pstxnQiTK7jtWc'
+ADMIN_ID = '7392310923'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
